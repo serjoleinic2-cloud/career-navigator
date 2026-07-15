068ca1d2c8c74b735cbdbf90df71bec7c9e74448
serjoleinic2-cloud <serjoleinic2@gmail.com>
2026-07-15 16:55:25 +0300

    feat(pm): add art.ts for Product Manager profession

---
+++ src/professions/product_manager/world/art.ts
@@ -0,0 +1,7 @@
+import { registerWorldArt } from '@/core/world/world_art_contract';
+
+registerWorldArt({
+  professionId: 'product_manager',
+  worldImageUrl: '/art/product_manager/journey.jpg',
+  worldImageSize: { width: 1080, height: 2340 },
+});
